f238a81ed90644a9b8012391fc811fb228c45a4c
Adheesh Juvekar <adheesh2@illinois.edu>
2025-12-19 07:19:27 -0500

    remove old files, update link

--- src/settings.ts
+++ src/settings.ts
@@ -41,7 +41,7 @@ export const social = {
 }
 
 export const template = {
-	website_url: 'https://localhost:4321', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
+	website_url: 'https://juvekaradheesh.github.io', // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
 	menu_left: false,
 	transitions: true,
 	lightTheme: 'bumblebee', // Select one of the Daisy UI Themes or create your own
